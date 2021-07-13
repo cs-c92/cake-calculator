2e81a1d61f2df47642a6018e734edee0f847d8a2
cs-c92 <81988507+cs-c92@users.noreply.github.com>
2021-07-13 20:55:32 +0100

    working scale sponge function
    it works

--- main.py
+++ main.py
@@ -2,24 +2,24 @@
 
 # ing for one cake
 eggs = 2
-flour_ounces = 4
+flour_ounces = 4 
 sugar_ounces = 4
 butter_ounces = 4
 bpow_tsp = 1
 
 def scale_sponge (num_of_cakes):
-    eggs = 2 * num_of_cakes
-    flour_ounces = 4 * num_of_cakes
-    sugar_ounces = 4 * num_of_cakes
-    butter_ounces = 4 * num_of_cakes
-    bpow_tsp = 1 * num_of_cakes
+    new_eggs = eggs * num_of_cakes
+    new_flour_ounces = flour_ounces * num_of_cakes
+    new_sugar_ounces = sugar_ounces * num_of_cakes
+    new_butter_ounces = butter_ounces * num_of_cakes
+    new_bpow_tsp = bpow_tsp * num_of_cakes
 
 
-    recipe = f'{num_of_cakes} Cake: {eggs} eggs, {flour_ounces} ounces of flour, {sugar_ounces} ounces of sugar, {bpow_tsp} teaspoons of baking powder'
+    recipe = f'{num_of_cakes} Cake: {new_eggs} eggs, {new_flour_ounces} ounces of flour, {new_sugar_ounces} ounces of sugar, {new_bpow_tsp} teaspoons of baking powder'
     print(recipe)
 
 
 
 
 
-scale_sponge(400)
+scale_sponge(6)
