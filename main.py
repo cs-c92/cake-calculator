9698c68ea579b749621c1e3f19e9a9cbb763ed17
cs-c92 <81988507+cs-c92@users.noreply.github.com>
2021-07-13 20:40:48 +0100

    multiplied
    multiplied ing

--- main.py
+++ main.py
@@ -7,3 +7,19 @@ sugar_ounces = 4
 butter_ounces = 4
 bpow_tsp = 1
 
+def scale_sponge (num_of_cakes):
+    eggs = 2 * num_of_cakes
+    flour_ounces = 4 * num_of_cakes
+    sugar_ounces = 4 * num_of_cakes
+    butter_ounces = 4 * num_of_cakes
+    bpow_tsp = 1 * num_of_cakes
+
+
+    recipe = f'{num_of_cakes} Cake: {eggs} eggs, {flour_ounces} ounces of flour, {sugar_ounces} ounces of sugar, {bpow_tsp} teaspoons of baking powder'
+    print(recipe)
+
+
+
+
+
+scale_sponge(400)
